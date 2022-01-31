bbee03b15070c103a1083da2b6a260733cac7da3
MOOX1 <vitormeneses87@gmail.com>
2022-01-30 21:47:00 -0300

    primeira versão do banco

---
+++ banco/src/ContaPolpanca.java
@@ -0,0 +1,10 @@
+
+
+public class ContaPolpanca extends Conta  {
+
+    
+
+}
+
+    
+
---
+++ banco/src/inicia.java
@@ -0,0 +1,20 @@
+
+
+public class inicia {
+
+    public static void main(String[] args) {
+
+       Conta contaCorrente = new ContaCorrente();
+       contaCorrente.depositar(100);
+       Conta contaPolpanca = new ContaPolpanca();
+
+       contaCorrente.transferir(100, contaPolpanca);
+
+
+       contaCorrente.imprimirExtrato();
+       contaPolpanca.imprimirExtrato();
+
+        
+    }
+    
+}
